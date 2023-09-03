ae4cb2c48a74c350de3735533f681d86e5a3efc2
ariefro <ariefromadhon26@gmail.com>
2023-09-03 20:54:08 +0700

    change login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,6 +3,7 @@ import { getProviders, signIn } from 'next-auth/react';
 import Image from 'next/image';
 import { useState } from 'react';
 import { HeadMeta, Loading } from '@/components';
+import Link from 'next/link';
 
 function Login({ providers }) {
   const [loading, setLoading] = useState(false);
@@ -21,13 +22,23 @@ function Login({ providers }) {
       ) : (
         <div className='flex flex-col items-center justify-center'>
           {showModal && (
-            <div className='absolute z-50 bg-zinc-300 top-0 bottom-0 left-0 right-0 bg-opacity-60 flex items-center justify-center'>
-              <div className='fixed text-sm z-50 top-24 text-zinc-100 bg-zinc-700 shadow-lg shadow-zinc-900 rounded-xl mx-10 max-w-2xl flex flex-col items-center justify-between px-10 py-7 md:text-base'>
+            <div className='absolute z-50 bg-zinc-600 top-0 bottom-0 left-0 right-0 bg-opacity-60 flex items-center justify-center'>
+              <div className='fixed text-sm z-50 top-1/4 text-zinc-100 bg-zinc-700 shadow-lg shadow-zinc-900 rounded-xl mx-10 max-w-2xl flex flex-col items-center justify-between p-12 md:text-base'>
                 <div>
                   <p>
-                    This app is currently in development. This app is only
-                    available to approved Spotify users. Please sign in to
-                    Spotify using the following credentials:
+                    This app uses the Spotify Web API. Spotify may use quotas
+                    and place use restrictions to ensure that the Spotify
+                    Platform is accessed and used as intended and in accordance
+                    with the Developer Agreement. Only authorized Spotify users
+                    have access to this app.{' '}
+                    <span>
+                      <Link className='text-green-400' href='/contact'>
+                        Contact us
+                      </Link>
+                    </span>{' '}
+                    if you would like to access this app with your Spotify
+                    account, or you can sign in to Spotify using the following
+                    credentials:
                   </p>
                   <br />
                   <div>
@@ -37,7 +48,7 @@ function Login({ providers }) {
                     </div>
                     <div className='flex'>
                       <p className='w-20'>Password</p>
-                      <p>: dummy999</p>
+                      <p>: secret123</p>
                     </div>
                   </div>
                 </div>
